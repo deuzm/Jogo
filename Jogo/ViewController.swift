369fbb3dcccb022a1939efa18f6d546897e4a81c
deuzm <elizabethmalin@icloud.com>
2020-03-02 18:48:39 +0300

    Initial Commit

---
+++ Jogo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Jogo
+//
+//  Created by Лиза  Малиновская on 3/2/20.
+//  Copyright © 2020 Лиза  Малиновская. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
